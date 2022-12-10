f61b1cf29c729be99d29bed84e0d28db10e636b6
July <106986983+July-vilh@users.noreply.github.com>
2022-12-10 16:39:20 +0300

    CREATE FILE

---
+++ Part_5.java
@@ -0,0 +1,35 @@
+Loops
+1. Loops allow you to repeat a block of code multiple times.
+
+For example, a banking app can loop over all bank transactions and check for some conditions.
+ 
+2. while Loops
+A while loop statement repeatedly executes a target statement as long as a given condition is true.
+
+Example:
+
+int x = 3;
+
+while(x > 0) {
+  System.out.println(x);
+  x = x-1;
+}
+
+RESULT: 3
+        2
+        1
+
+The while loops check for the condition x > 0. If it evaluates to true, it executes the statements within its body. Then it checks for the statement again and repeats.
+The code above will output the numbers 3 to 1, and then stop, as the condition will become false, after x reaches 0.
+
+3. 
+  
+int x = 3;
+
+while(x > 0) {
+  System.out.println(x);
+  x = x-1;
+}
+
+The line x = x-1; is important, as without it the condition would never become false and the loop would run forever. 
+Each time the loop runs, 1 is subtracted from x.
