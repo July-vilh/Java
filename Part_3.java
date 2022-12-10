ae31f764051f6ec097997da7c23ac67f230d5ee6
July <106986983+July-vilh@users.noreply.github.com>
2022-12-10 15:50:11 +0300

    UPDATE FILE

--- Part_3.java
+++ Part_3.java
@@ -1,4 +1,4 @@
-switch
+1. switch
 Instead of many if else statements, which become hard to read, we can use a switch statement.
 
 A switch statement tests a variable for equality against a list of values.
@@ -19,8 +19,8 @@ switch(day) {
     break;
 }
 
-
-It is important to have a break statement for each case.
+____
+2. It is important to have a break statement for each case.
 If no break appears, the program will continue to execute the next case in the switch, even if the value does not match the variable that is switched on.
 Run this example to see what happens when there is no break in the case:
 
@@ -34,3 +34,29 @@ switch(day) {
   case 3:
     System.out.println("Wednesday");
 }
+
+RESULT: Tuesday
+        Wednesday
+
+____
+3. The default Case
+A switch statement can have an optional default case.
+Оператор switch может иметь необязательный регистр по умолчанию.
+
+ int day = 5;
+
+        switch(day) {
+        case 1:
+            System.out.println("Monday");
+        case 2:
+            System.out.println("Tuesday");
+        case 3:
+            System.out.println("Wednesday");
+        default: 
+            System.out.println("Another day");
+        }
+    }
+}
+
+RESULT: Another day
+
