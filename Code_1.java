f91dc51e3b95a40d683507c29e9262f440c4ecbc
July <106986983+July-vilh@users.noreply.github.com>
2022-12-12 20:40:55 +0300

    update file

--- Code_1.java
+++ Code_1.java
@@ -156,3 +156,18 @@ System.out.println(menu[choice]);
     }
   }
 }
+__________________________
+7. The given code declares an array that holds the monthly revenues for a company for a year.
+You need to calculate the average monthly revenue for the year. For that, calculate the sum of the revenue for all the months and divide it by the number of items in the array.
+ 
+ public class Program {
+    public static void main(String[] args) {
+        double[] revenue = {88750, 125430, 99700, 14500, 158000, 65000, 99000, 189000, 210000, 42000, 165800, 258900};
+        double sum = 0;
+
+        for (double x : revenue) {
+            sum += x;
+        }
+        System.out.println(sum / 12);
+    }
+}
