99e1fa218d28a87681508396853d87f0801f6197
July <106986983+July-vilh@users.noreply.github.com>
2022-12-12 11:52:23 +0300

    update file

--- code1.java
+++ code1.java
@@ -21,6 +21,7 @@ public class Main {
   }
 }
 
+
 2. You are making a robot that should categorize items by their color. Each color corresponds to a box with a specific number.
 For simplicity, our program will handle 3 colors:
 red goes to box #1
@@ -54,3 +55,60 @@ public class Main {
     }
   }
 }
+
+
+3. Given the age of a person as an input, output their age group. Here are the age groups you need to handle:
+Child: 0 to 11
+Teen: 12 to 17
+Adult: 18 to 64
+
+Sample Input
+42
+
+Sample Output
+Adult
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int age = in.nextInt();
+
+    if (age >= 0 && age <= 11) {
+      System.out.println("Child");
+    } else if (age >= 12 && age <= 17) {
+      System.out.println("Teen");
+    } else if (age >= 18 && age <= 64) {
+      System.out.println("Adult");
+    }
+  }
+}
+
+
+4. Your math teacher asked you to calculate the sum of the numbers 1 to N, where N is a given number.
+Task: Take an integer N from input and output the sum of the numbers 1 to N, inclusive.
+
+Sample Input:
+10
+
+Sample Output:
+55
+  
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int n = in.nextInt();
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+      sum += i;
+    }
+
+    System.out.println(sum);
+  }
+}
+
