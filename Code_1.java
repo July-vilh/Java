c05465b27cf8eaff1d1a98f3fece4a5d305c56e8
July <106986983+July-vilh@users.noreply.github.com>
2022-12-17 21:09:08 +0300

    update file

--- Code_1.java
+++ Code_1.java
@@ -171,3 +171,30 @@ You need to calculate the average monthly revenue for the year. For that, calcul
         System.out.println(sum / 12);
     }
 }
+___________
+8. You are creating a ticketing program for a small movie theater.
+The seats are represented using a 2-dimensional array. Each item can have the values 1 and 0 - 1 is occupied, and 0 if it's free.
+Your program needs to take as input the row and the column of the seat and output Free if it's free, and Sold if it's not.
+  
+import java.util.Scanner;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        int[][] seats = {
+        {0, 0, 0, 1, 1, 1, 0, 0, 1, 1},
+        {1, 1, 0, 1, 0, 1, 1, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 1, 0, 0, 0, 1, 1, 1}
+        };
+        Scanner sc = new Scanner(System.in);
+     
+    int row = sc.nextInt();
+    int column = sc.nextInt();
+
+    if(seats[row][column] == 0){
+        System.out.println("Free");
+    } else System.out.println("Sold");
+  }
+}
