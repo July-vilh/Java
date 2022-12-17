c4d5746e6fca44349a3b901294ed367549a48f57
July <106986983+July-vilh@users.noreply.github.com>
2022-12-17 21:51:01 +0300

    update file

--- Code_1.java
+++ Code_1.java
@@ -198,3 +198,39 @@ public class Program {
     } else System.out.println("Sold");
   }
 }
+__________
+9. You are making an automated response program for a store. The bot should take a number from the user as input and reply with an automated message.
+There are currently 3 responses, that you need to a handle:
+- User message: "1", Reply: "Order confirmed"
+- User message: "2", Reply: "info@sololearn.com"
+For any other number, the reply should be: "Try again".
+The given code calls a method called bot(). Define the method, which should take an integer input from the user, and handle the above mentioned cases, by outputting the corresponding reply.
+Do not change the method call in main().
+
+import java.util.Scanner;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        bot();
+    }
+
+    public static void bot(){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        
+         switch (number) {
+    case 1:
+    
+      System.out.println("Order confirmed");
+      break;
+    case 2:
+
+      System.out.println("info@sololearn.com");
+      break;
+    default:
+      System.out.println("Try again");
+      break;
+}
+    }
+}
