6844f494bc3cf56a60b7a0a02e7b8a826170fa9c
July <106986983+July-vilh@users.noreply.github.com>
2023-01-29 16:08:55 +0300

    update file

--- theory/EPAM. Функции.java
+++ theory/EPAM. Функции.java
@@ -14,6 +14,20 @@ ______________________
 !!!!!!!!!!!!!!! Данная функция может использоваться для вывода целого массива в консоль !!!!!!!!!!!!!!!
 ! То есть вызывается метод, в него печататется массив и далее этот массив выводится в консоль ! При этом не ожидается что вернется обратно какой-то результат например в виде подсчета чего-то
 
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {0, 9, 9, 7};
+        printArrayToConsole(array);
+    }
+        static void printArrayToConsole(int[] arrayToPrint){
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.println(arrayToPrint[i]);
+        }
+
+    }
+}
+
+* ТУТ arrayToPrint.length ЭТО ДЛИНА МАССИВА
 
 _______________________
 
@@ -26,4 +40,47 @@ ________________________________________________
 ----------------------------------------         4. double sum(double a, double b)     -------------------------------------------
   
 Данная функция принимает несколько параметров и как итог выводит подсчет (значение) в консоль
+
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = {0, 9, 9, 7};
+        printArrayToConsole(array);
+        double sum = 0.0;
+        sum = sumDouble(a: 1.0, b: 2.0);
+        System.out.println(sum);
+    }
+    static double sumDouble(double a, double b) {
+        double sum = a + b;
+        return sum;
+    }
+        
+        
+        
+___________________________________________________ --- КОМБИ ---- ______________________________________________
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = {0, 9, 9, 7};
+        printArrayToConsole(array);
+        double sum = 0.0;
+        sum = sumDouble(a: 1.0, b: 2.0);
+        System.out.println(sum);
+    }
+    static double sumDouble(double a, double b) {
+        double sum = a + b;
+        return sum;
+    }
+    static void printArrayToConsole(int[] arrayToPrint){
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.println(arrayToPrint[i]);
+        }
+
+    }
+}
+
+ФУНКЦИИ МОГУТ БЫТЬ ОБЪЕДИНЕНЫ В НАБОР ФУНКЦИЙ (БИБЛИОТЕКУ) ПО ТЕМАТИКЕ 
+
+
+
   
