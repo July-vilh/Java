814490cd74b4f5cca2a167df1f435cb5f48e8942
July <106986983+July-vilh@users.noreply.github.com>
2023-01-30 15:37:53 +0300

    update file

--- EPAM. Вывод макс. и мин. эл. массива.java
+++ EPAM. Вывод макс. и мин. эл. массива.java
@@ -2,6 +2,8 @@ ________________________________________________________________________________
 
 -------------------------------------------  Код на вывод максимального и минимального значения в массиве ----------------------------------------------------
 
+----------------------------------------------------- НА ОСНОВЕ МАЛЕНЬКОГО МАССИВА ---------------------------------------------------------------
+    
 public class Main {
     public static void main(String[] args) {
         int [] arraySmall = {20, 91, 64, 75, 19, 26, 48, 55, 89};
@@ -75,3 +77,43 @@ Max element: 91
 Max element index: 1
 Comparisons: 9
 Replacements: 1
+
+_____________________________________________________________________________________________________________________________________________________
+
+----------------------------------------- НА ОСНОВЕ ДЛИННОГО МАССИВА ------------------------------------------------------------------------------
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arraySmall = {20, 91, 64, 75, 19, 26, 48, 55, 89};
+        int [] arrayRandom = {70, 79, 98, 13, 75, 96, 24, 36, 88, 85, 47, 85, 56, 32, 5, 69, 86, 45, 82};
+        printMaximumElement(arrayRandom);
+
+    }
+    static void printMaximumElement(int[] array) {
+        int max = array[0];
+        int maxIndex = 0;
+        int comparisons = 0;
+        int replacements = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+                replacements++;
+            }
+            comparisons++;
+        }
+        System.out.println("Max element: " + max);
+        System.out.println("Max element index: " + maxIndex);
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Replacements: " + replacements);
+    }
+}
+
+_________________________________
+
+ИТОГ:
+
+Max element: 98
+Max element index: 2
+Comparisons: 19
+Replacements: 2
